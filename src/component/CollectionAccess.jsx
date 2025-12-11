5ad89646ff120c7f6d7a00f2e62b66a9017ef815
Shunnn19 <Shunnn19@users.noreply.github.com>
2025-12-11 16:37:49 +0800

    Try

---
+++ src/component/CollectionAccess.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Greeting({ name }) {
+    return (
+        <div>
+            <h1>Hello, {name}!</h1>
+            <p>Welcome to our website.</p>
+        </div>
+    );
+}
+
+export default Greeting;
